552689cbd72b7a9b590dcb696d21e87b1f00a5ab
Mark <wixw1cky7@gmail.com>
2025-07-31 18:28:04 +0700

    [backend] Update configs and add action

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     Project dependencies config
     """
     model_config = SettingsConfigDict(
-        # env_file=f'{BASE_DIR}/.env',
+        env_file=f'{BASE_DIR}/.env',
         extra='ignore'
     )
     
@@ -28,29 +28,12 @@ class Settings(BaseSettings):
     REFRESH_TTL: int = 60 * 60 * 24 * 7
     CSRF_HMAC_KEY: bytes
     
-    # # Database settings
-    # DB_USER: str
-    # DB_PASSWORD: SecretStr
-    # DB_HOST: str
-    # DB_PORT: int
-    # DB_NAME: str
-    
-    # # Redis settings
-    # REDIS_HOST: str
-    # REDIS_PORT: int
+    # Database settings
+    DATABASE_URL: str
+    REDIS_URL: str
 
     # Site data (url, paths)
     SITE_URL: str = ''
-    
-    DATABASE_URL: str
-    REDIS_URL: str
-    
-    # @property
-    # def DB_URI(self) -> str:
-    #     return (
-    #         f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}"
-    #         f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    #     )
 
 
 def configure_logging():
