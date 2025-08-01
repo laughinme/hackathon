960175b3bd6af274129d293c621dff9753887892
Oatis123 <oatgrom@gmail.com>
2025-08-01 18:52:15 +0700

    [mobile] feat: Add OnboardingViewModel
    - Initializes an `OnboardingViewModel` using Hilt for dependency injection.
    - Manages email and password input via `MutableStateFlow`.
    - Includes placeholder methods for sign-in and sign-up logic.
    - Adds TODOs for future implementation with UseCases.

--- mobile/app/src/main/java/com/example/hackathon/presentation/viewmodel/OnboardingViewModel.kt
+++ mobile/app/src/main/java/com/example/hackathon/presentation/viewmodel/OnboardingViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.hackathon.presentation.viewmodel
 
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor() {
+
+    private val _email = MutableStateFlow("")
+    val email = _email.asStateFlow()
+
+    private val _password = MutableStateFlow("")
+    val password = _password.asStateFlow()
+
+    fun onEmailChanged(newEmail: String) {
+        _email.value = newEmail
+    }
+
+    fun onPasswordChanged(newPassword: String) {
+        _password.value = newPassword
+    }
+
+    fun sighIn() {}//TODO реалищовать с UseCase
+
+    fun signUp() {}//TODO реалищовать с UseCase
+
+
+}
