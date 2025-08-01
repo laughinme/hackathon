1b90945746d5b44abde2e36f91b570a84d8bfe8f
Michael Lapcevich <149459288+lapcevichme@users.noreply.github.com>
2025-08-01 15:47:54 +0700

    [mobile] feat: implemented WheelPickerCompose
    Imported lib from github, made basic preview for AgePickerScreen.
    
    Changed min api to 26!

--- mobile/app/src/main/java/com/example/hackathon/presentation/screens/onboardingScreens/GenderPickerScreen.kt
+++ mobile/app/src/main/java/com/example/hackathon/presentation/screens/onboardingScreens/GenderPickerScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,16 +17,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.PreviewTheme
 
 @Composable
-fun GenderPickerscreen() {
+fun GenderPickerScreen() {
     val genders = listOf("Male", "Female", "Other")
     //var selectedGender TODO реализовать эту переменную во ViewModel
-    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.Center) {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Box(modifier = Modifier.weight(1f))
         Box(
             modifier = Modifier.weight(1f),
@@ -78,6 +76,6 @@ fun GenderPickerscreen() {
 @Composable
 fun GenderPickerPreview() {
     PreviewTheme {
-        GenderPickerscreen()
+        GenderPickerScreen()
     }
 }
